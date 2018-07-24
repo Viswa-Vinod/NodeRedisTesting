873dda4b42c37140c407d90e3a82aaaed15bff6f
Vinod Kumar <vkviswa@gmail.com>
2018-07-24 21:08:50 +0530

    finish redis section half-way

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,26 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:8000';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+    
+    //do we have any data related to this query. If yes, return the value and return from the request
+
+    const cachedBlogs = await client.get(req.user.id);
+    if (cachedBlogs) {
+      console.log('serving from cache');
+      return res.send(JSON.parse(cachedBlogs));
+    } 
+    //if no, send request to mongo and update cache with result
+
     const blogs = await Blog.find({ _user: req.user.id });
 
+    console.log('serving from mongoDB');
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs))
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
