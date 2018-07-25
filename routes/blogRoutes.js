730fbb7746d548ddb3959916866fe3e06d6e9958
Vinod Kumar <vkviswa@gmail.com>
2018-07-25 14:18:40 +0530

    add redis caching

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-
-const Blog = mongoose.model('Blog');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const cleanCache = require('../middlewares/cleanCache');
+const Blog = mongoose.model("Blog");
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,30 +13,14 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const redisUrl = 'redis://127.0.0.1:8000';
-    const client = redis.createClient(redisUrl);
-    const util = require('util');
-    client.get = util.promisify(client.get);
-    
-    //do we have any data related to this query. If yes, return the value and return from the request
-
-    const cachedBlogs = await client.get(req.user.id);
-    if (cachedBlogs) {
-      console.log('serving from cache');
-      return res.send(JSON.parse(cachedBlogs));
-    } 
-    //if no, send request to mongo and update cache with result
-
-    const blogs = await Blog.find({ _user: req.user.id });
-
-    console.log('serving from mongoDB');
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
     res.send(blogs);
-    client.set(req.user.id, JSON.stringify(blogs))
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -51,5 +35,6 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
+    
   });
 };
